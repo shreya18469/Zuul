390619f802a3c46799d00fa8c53d08477bcd31c6
Shreya Suresh <shreya4763@gmail.com>
2022-12-12 13:33:46 -0800

    Exits and setting up inventory

--- main.cpp
+++ main.cpp
@@ -10,6 +10,30 @@ void pickObject(Room* room, vector <Objects*>* inventory){
 void dropObject(Room* room, vector <Objects*>* inventory){
 }
 void roomDetail(Room* room, vector <Objects*>* inventory){
+  cout << You are in the " << room->getType() << endl;
+  cout << "The objects in the room are: ";
+  vector <Objects*>* objectlist = room->getObjectlist();
+  for (int i = 0; i < objectlist->size(); i++){
+    Objects* o = (*objectlist)[i];
+    cout << 0->getObject() << " ";
+  }
+  cout << endl;
+  cout << "In your inventory, you have: ";
+  for (int i = 0; i < inventory->size(); i++){
+    Obejcts* o = (*inventory)[i];
+    cout << o->getObject() << " ";
+  }
+  cout << endl;
+  cout << "Exists: ";
+  if (room->getEast() != NULL){
+    cout << "East " << endl;
+  } if (room->getWest() != NULL){
+    cout << "West " << endl;
+  } if (room->getNorth() != NULL){
+    cout << "North " << endl;
+  } if (room->getSouth() != NULL){
+    cout << "South " << endl;
+  }
 }
 int main(){
   Room* office = new Room();
@@ -47,6 +71,41 @@ int main(){
   art->addObject("paintbrush");
   Room* dance = new Room();
   dance>setType("Dance");
+
+  office->setEast(cafeteria);
+  office->setNorth(auditorium);
+  auditorium->setSouth(office);
+  auditorium->setEast(gym);
+  gym->setWest(auditorium);
+  gym->setEast(history);
+  gym->setSouth(bathroom);
+  gym->setNorth(cafeteria);
+  bathroom->setSouth(gym);
+  history->setWest(gym);
+  history->setEast(math);
+  math->setWest(history);
+  math->setSouth(marketing);
+  marketing->setNorth(math);
+  marketing->setWest(computerlab);
+  computerlab->setEast(marketing);
+  computerlab->setWest(cafeteria);
+  cafeteria->setWest(office);
+  cafeteria->setNorth(gym);
+  cafeteria->setEast(computerlab);
+  cafeteria->setSouth(french);
+  french->setEast(japanese);
+  french->setWest(biolab);
+  japanese->setWest(french);
+  biology->setEast(french);
+  biology->setSouth(art);
+  CAD->setNorth(french);
+  CAD->setWest(art);
+  art->setEast(CAD);
+  art->setWest(dance);
+  art->setNorth(biolab);
+  dance->setEast(art);
+
+  vector <Objects*>* inventory = new vector <Objects*>();
   
   return 0;
 }
--- rooms.cpp
+++ rooms.cpp
@@ -6,6 +6,18 @@
 
 using namespace std;
 
+void Objects::setObjects(char o[]){
+  strcpy(object, o);
+}
+char* Objects::getObject(){
+  return object;
+}
+void Room::setType(char t[]){
+  strcpy(type, t);
+}
+char* Room::getType(){
+  return type;
+}
 void Room::setEast(Room* room){
 
 }
@@ -28,5 +40,27 @@ void Room::setSouth(Room* room){
 
 }
 Room* Room::getSouth(){
+
+}
+
+vector <Objects*>* Room::getObjectList(){
+  return objectlist;
+}
+
+Room::Room(){
+  objectList = new vector<Objects*>();
+  exits = new map<int, Room*>();
+}
+void Room::addObject(char ob[]){
+  Objects* o = new Objects();
+  o->setObject(ob);
+
+  if (objectList == NULL){
+    cout << "No object list" << endl;
+  }
+  objectList->push_back(o);
+}
+
+Objects* Room::deleteObject(char ob[]){
 
 }
